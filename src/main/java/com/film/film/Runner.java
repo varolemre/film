9e31f916b0cede2004f0da18c73072913583d247
varolemre <40075749+varolemre@users.noreply.github.com>
2020-07-09 23:47:42 +0300

    runner command update

--- src/main/java/com/film/film/Runner.java
+++ src/main/java/com/film/film/Runner.java
@@ -2,10 +2,13 @@ package com.film.film;
 
 import com.film.film.model.Actor;
 import com.film.film.model.Film;
+import com.film.film.model.User;
 import com.film.film.repository.ActorRepository;
 import com.film.film.repository.FilmRepository;
+import com.film.film.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -14,6 +17,8 @@ import java.util.List;
 @Component
 public class Runner implements CommandLineRunner {
 
+    @Autowired
+    UserRepository userRepository;
     @Autowired
     ActorRepository actorRepository;
     @Autowired
@@ -35,5 +40,18 @@ public class Runner implements CommandLineRunner {
         actorRepository.save(actor2);
 
         filmRepository.save(film);
+
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword(new BCryptPasswordEncoder().encode("fbfbfb333"));
+        user.setActive(true);
+        user.setRole("ROLE_ADMIN");
+        userRepository.save(user);
+        User user2 = new User();
+        user2.setUsername("user");
+        user2.setPassword(new BCryptPasswordEncoder().encode("fbfbfb333"));
+        user2.setActive(true);
+        user2.setRole("ROLE_USER");
+        userRepository.save(user2);
     }
 }
